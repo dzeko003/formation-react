bd1dd0a19df1f77ba9855813539b6a2bdd16a124
dzeko003 <berenismassamba@gmail.com>
2024-04-03 09:29:01 +0200

    premier commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const title = "Bonjour les gens";
+
+function App() {
+  return (
+    <>
+      <h1 id="title" className="title">
+        {title}
+      </h1>
+      <input type="text" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum alias
+        optio totam, vel harum autem saepe adipisci amet cumque et nam maxime
+        aliquam eius eum explicabo fuga. Explicabo, illo veniam.
+      </p>
+    </>
+  );
+}
+
+export default App;
