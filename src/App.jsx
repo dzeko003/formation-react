34cad0286bf1331c441c45683369479f033cdf0b
dzeko003 <berenismassamba@gmail.com>
2024-04-03 16:09:18 +0200

    comprendre le hook use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,33 @@
-const title = "Bonjour les gens";
-
-const style = { color: "red", backgroundColor: "blue" };
-
-const showTitle = false;
-
-const todos = [
-  'présenter rect',
-  'Presenter le jsx',
-  'créer des compsants'
-]
+import { useState } from "react";
 
 function App() {
-  const handleClick = (e) => {
-    console.log(e);
-    alert(" J'ai cliqué sur le titre");
-  };
+
+  const [count , setCount] = useState(0)
+  
+    const [person , setPerson] = useState({
+      firstName : 'john',
+      lastName : 'Doe',
+      age : 18
+    })
+
+
+  const incrementAge = () =>{
+
+    setPerson({...person , age:person.age +1})
+
+  }
+
+  const incrementCount = () =>{
+      setCount(count +1)
+  }
 
   return (
     <>
-      <Title color='green' id='greenTitle' className='green'>Les congolaises sont belles</Title>
-      {showTitle ?
-        (
-        <h1 onClick={handleClick} id="title" className="title" style={style}>
-          {title}
-        </h1>
-        ) :
-        
-        (
-        <p>Pas de title</p>
-        )
-      }
-
-      <input type="text" />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum alias
-        optio totam, vel harum autem saepe adipisci amet cumque et nam maxime
-        aliquam eius eum explicabo fuga. Explicabo, illo veniam.
-      </p>
-
-      <ul>
-        {todos.map(todo => (<li key={todo}>{todo}</li>))}
-      </ul>
+      <p> Age de {person.firstName} : {person.age}</p>
+      <button onClick={incrementAge}>Gagner age</button>
+      <button onClick={incrementCount}>Increment count {count}</button>
     </>
   );
-}
-
-function Title({color , children , hidden , ...props}){
-  if(hidden){
-    return null
-  }
-  return <h1 style={{color:color}} {...props}>{children}</h1>
 }
 
 export default App;
