1b050bf582c360316badb6ffd7ac21c0f6432a7d
dzeko003 <berenismassamba@gmail.com>
2024-04-09 11:22:02 +0200

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,118 +1,33 @@
 import { useState } from "react";
-import { Input } from "./components/forms/Input.jsx";
-import { Checkbox } from "./components/forms/Checkbox";
-import { ProductCategoryRow } from "./components/products/ProductCategoryRow.jsx";
-import { ProductRow } from "./components/products/ProductRow.jsx";
-
-const PRODUCTS = [
-  {
-    category: "Fruits",
-    price: "$1",
-    stocked: true,
-    name: "Apple",
-  },
-  {
-    category: "Fruits",
-    price: "$1",
-    stocked: true,
-    name: "DragonFruits",
-  },
-  {
-    category: "Fruits",
-    price: "$1",
-    stocked: false,
-    name: "PassionFruit",
-  },
-  {
-    category: "Vegetables",
-    price: "$1",
-    stocked: true,
-    name: "spinach",
-  },
-  {
-    category: "Vegetables",
-    price: "$1",
-    stocked: false,
-    name: "Pumpkin",
-  },
-  {
-    category: "Vegetables",
-    price: "$1",
-    stocked: true,
-    name: "Péas",
-  },
-];
+import { Input } from "./components/forms/input";
 
 function App() {
-  const [showStockedOnly, setShowStockedOnly] = useState(false);
-  const  [search , setSearch] = useState('')
+  return (
+    <>
+      <EditTitle />
+    </>
+  );
+}
 
-  const visibleProducts = PRODUCTS.filter(product => {
-    if( showStockedOnly && !product.stocked){
-      return false
-    }
-
-    if( search && !product.name.includes(search)){
-      return false
-    }
-
-    return true
-  })
-
+function EditTitle() {
+  const [title, setTitle] = useState("");
+  const [firstname, setFirstName] = useState("");
 
   return (
-    <div className="container my-3">
-      <SearchBar
-        search = {search}
-        onSearchCange = {setSearch()}
-        showStockedOnly={showStockedOnly}
-        onStockOnlyChange={setShowStockedOnly}
+    <div className="vstack gap-2">
+      <Input
+        type="text"
+        placeholder="Modifier le titre"
+        value={title}
+        onChange={setTitle}
       />
-      <ProductTable products={visibleProducts} />
+      <Input
+        type="text"
+        placeholder="Modifier le prénom"
+        value={firstname}
+        onChange={setFirstName}
+      />
     </div>
-  );
-}
-
-function SearchBar({ showStockedOnly, onStockOnlyChange , search , onSearchCange }) {
-  return (
-    <div>
-      <div className="mb-3">
-        <Input value={search} onChange={onSearchCange} placeholder="Rechercher..." />
-        <Checkbox
-          type="checkbox"
-          id="stock "
-          checked={showStockedOnly}
-          label="N'affichez que les produits en stock"
-          onChange={onStockOnlyChange}
-        />
-      </div>
-    </div>
-  );
-}
-
-function ProductTable({ products }) {
-  const rows = [];
-  let lastcategory = null;
-
-  for (let product of products) {
-    if (product.category !== lastcategory) {
-      rows.push(
-        <ProductCategoryRow name={product.category} key={product.category} />
-      );
-    }
-    lastcategory = product.category;
-    rows.push(<ProductRow product={product} key={product.name} />);
-  }
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Nom</th>
-          <th>Prix</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
   );
 }
 
