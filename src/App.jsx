b1ba33583d5243cd2894878f2b2cb399c8b052d7
dzeko003 <berenismassamba@gmail.com>
2024-04-13 16:36:40 +0200

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState , memo } from "react";
 import { Input } from "./components/forms/Input.jsx";
 import { Checkbox } from "./components/forms/Checkbox.jsx";
 import { useIncrement } from "./hooks/useIncrement.js";
 import { useDocumentTitle } from "./hooks/useDocumentTitle.js";
 import { useFetch } from "./hooks/useFetch.js";
 
-
-
 function App() {
-  
-  const {loading , data , errors} = useFetch("https://jsonplaceholder.typicode.com/posts?_limit=10&_delay=5000");
+  const [name, setName] = useState("");
 
-  return <div>
-        {loading && <div>Chargement...</div>}
-        {errors && <div className="alert alert-danger">{errors.toString()}</div>}
-        {data && <div>
-            <ul>
-              {data.map(post =>(<li key={post.id}>{post.title}</li>))}
-            </ul>
-          </div>}
+  // console.log(
+  //   <div className="container my-2 vstack gap-2">
+  //     <div>
+  //       <Input label="Prénom" onChange={setName} value={name} />
+  //       <div>{name.toUpperCase()}</div>
+  //     </div>
+  //   </div>
+  // );
+
+  console.log('App' , 'render');
+
+  return <div className="container my-2 vstack gap-2">
+    <div>
+      <Input label="Prénom" onChange={setName} value={name}/>
+      <div>
+        {name.toUpperCase()}
+      </div>
+    </div>
+    <InfoMemo/>
   </div>
 }
 
+const InfoMemo = memo( function Info(){
+  // waitSync(500)
+  console.log('Info' , 'render');
+  return <div className="alert alert-info">
+    Lorem ipsum dolor, sit amet consectetur adipisicing
+     elit. Eligendi porro, laudantium eius perspiciatis ut
+      vel dolorem provident veniam cumque corrupti libero nobis molestias delectus quis voluptatum at aliquam, aut quidem!
+  </div>
+})
 export default App;
