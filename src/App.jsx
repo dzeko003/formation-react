23cda15cae2b2d3bcd97246df6de1d847245834c
dzeko003 <berenismassamba@gmail.com>
2024-04-16 22:38:27 +0200

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState, memo, useCallback } from "react";
+import { useEffect, useMemo, useRef, useState, memo, useCallback, lazy, Suspense } from "react";
 import { Input } from "./components/forms/Input.jsx";
 import { Checkbox } from "./components/forms/Checkbox.jsx";
 import { useIncrement } from "./hooks/useIncrement.js";
@@ -7,60 +7,35 @@ import { useFetch } from "./hooks/useFetch.js";
 import { createPortal } from "react-dom";
 
 function App() {
+  const {page , param} = useHashNavigation();
+  const getPageContent = getPageContent(page, param);
+
   return (
-    <div
-      style={{
-        height: 300,
-        overflowY: "scroll",
-        background: "#EEE",
-        margin: 20,
-        position: "relative",
-      }}
-    >
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
-        temporibus recusandae doloremque odit iusto explicabo expedita modi
-        optio dolorem delectus! Pariatur, repellat aut. Neque autem possimus
-        odit quasi iure non magnam ut voluptates, iusto saepe sapiente id cum
-        hic rem deleniti cupiditate amet quidem, aliquid nemo pariatur. Nam,
-        assumenda excepturi.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
-        temporibus recusandae doloremque odit iusto explicabo expedita modi
-        optio dolorem delectus! Pariatur, repellat aut. Neque autem possimus
-        odit quasi iure non magnam ut voluptates, iusto saepe sapiente id cum
-        hic rem deleniti cupiditate amet quidem, aliquid nemo pariatur. Nam,
-        assumenda excepturi.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
-        temporibus recusandae doloremque odit iusto explicabo expedita modi
-        optio dolorem delectus! Pariatur, repellat aut. Neque autem possimus
-        odit quasi iure non magnam ut voluptates, iusto saepe sapiente id cum
-        hic rem deleniti cupiditate amet quidem, aliquid nemo pariatur. Nam,
-        assumenda excepturi.
-      </p>
-      <Modal />
-    </div>
+    <>
+      <Header page={page} />
+      <div className="container my-2">{pageContent}</div>
+    </>
   );
 }
 
-function Modal() {
-  return createPortal(
-    <div
-      style={{
-        position: "absolute",
-        top: 0,
-        right: 0,
-        padding: 10,
-        border: "solid 1px grey",
-        background: "#FFF",
-      }}
-    >
-      Je suis une modale
-    </div> , document.body
-  );
+function getPageContent(page, param) {
+
+  if (page === "home") {
+    return <Home />;
+  }
+
+  if (page === "post") {
+    const SingleLazy = lazy(() => import('./pages/Single'))
+    return <Suspense fallback={<div>Chargement des composants en cours</div>}>
+      <SingleLazy postId={param} />;
+      </Suspense>
+  }
+
+  if (page === "contact") {
+    return <Contact />;
+  }
+
+  return <NotFound page={page} />;
 }
 
 export default App;
