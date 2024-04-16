8f825e93c6acf29d8cccafa17501a8bb4b404b9a
dzeko003 <berenismassamba@gmail.com>
2024-04-16 23:30:14 +0200

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
-import { useEffect, useMemo, useRef, useState, memo, useCallback, lazy, Suspense } from "react";
+import {
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+  memo,
+  useCallback,
+  lazy,
+  Suspense,
+  useReducer,
+} from "react";
 import { Input } from "./components/forms/Input.jsx";
 import { Checkbox } from "./components/forms/Checkbox.jsx";
 import { useIncrement } from "./hooks/useIncrement.js";
@@ -6,36 +16,61 @@ import { useDocumentTitle } from "./hooks/useDocumentTitle.js";
 import { useFetch } from "./hooks/useFetch.js";
 import { createPortal } from "react-dom";
 
-function App() {
-  const {page , param} = useHashNavigation();
-  const getPageContent = getPageContent(page, param);
+function reducer(state, action) {
+  if (action.type === "REMOVE_TODO") {
+    return {
+      ...state,
+      todos: state.todos.filter((todo) => todo.name !== action.payload.name),
+    };
+  }
 
-  return (
-    <>
-      <Header page={page} />
-      <div className="container my-2">{pageContent}</div>
-    </>
-  );
+  if(action.type === 'TOGGLE_TODO'){
+    return{
+      ...state,
+      todos:state.todos.map(todo => todo === action.playLoad ? {
+        ...todo,
+        checked: !todo.checked
+      } : todo) 
+    }
+  }
+
+  return state;
 }
 
-function getPageContent(page, param) {
 
-  if (page === "home") {
-    return <Home />;
-  }
-
-  if (page === "post") {
-    const SingleLazy = lazy(() => import('./pages/Single'))
-    return <Suspense fallback={<div>Chargement des composants en cours</div>}>
-      <SingleLazy postId={param} />;
-      </Suspense>
-  }
-
-  if (page === "contact") {
-    return <Contact />;
-  }
-
-  return <NotFound page={page} />;
+function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    todos: [
+      {
+        name: "Faire les courses",
+        checked: false,
+      },
+      {
+        name: "Ranger les courses",
+        checked: false,
+      },
+      {
+        name: "Manger les courses",
+        checked: false,
+      },
+    ],
+  });
+  return (
+    <ul>
+      {state.todos.map((todo) => (
+        <li
+          key={todo.name}
+          onClick={null}
+        >
+          <input type="checkbox" onChange={() => dispatch(({type:'TOGGLE_TODO'}))} checked={todo.cheked}/>
+          {todo.name}
+          <button onClick={() => dispatch({ type: "REMOVE_TODO", payload: todo })}>
+            Supprimer
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
