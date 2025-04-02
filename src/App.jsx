73620b37c7a73e695158d798d50b8590655322b0
dzeko003 <berenismassamba@gmail.com>
2025-04-02 07:08:58 +0100

    la syntaxe Jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,72 @@
-const title = "Bonjour les gens";
-
-const style = { color: "red", backgroundColor: "blue" };
-
-const showTitle = false;
-
-const todos = [
-  'présenter rect',
-  'Presenter le jsx',
-  'créer des compsants'
-]
 
 function App() {
-  const handleClick = (e) => {
-    console.log(e);
-    alert(" J'ai cliqué sur le titre");
-  };
+ 
+  /**
+   * hormis les attributs comme aria.... ou data.... les autres s'écrivent en kamlekaze
+   * 
+   * IL ne peut y avoir qu'un element racine
+   * 
+   * l'attribut class est remplacé par className
+   * 
+   * <input> est faux il va falloir le fermer comme ceci <input/> ou bien <input></input>
+   * 
+   * Il y a aussi la notion d'interpollation ( on déclare une variable title = "Bonjour les gens" et on l'appelle)
+   *
+   * Le style doit être passé comme un objet dans l'interpolation (les attributs comme backhourd-color s'écrivent backgroudColor)
+   * 
+   * Les attribut qui font appelle aux evenement comme onClick , onChange sont appellé comme suit <h1 onClick={handleClick}></h1> il ne faut pas ecrire 
+   * <h1 onClick={handleClick}></h1> sinon il sera appelé des que le composant sera rendu.
+   * 
+   * Logique conditionnelle showTitle = false ex {showTitle && <h1 />}
+   * 
+   * const todos = ["alpha" "beta" , "gamma"]
+   * 
+   * On peut créer d'autres composant qui deviennent comme des balises personnalisées.
+   * 
+   * 
+   */
 
   return (
     <>
-      <Title color='green' id='greenTitle' className='green'>Les congolaises sont belles</Title>
-      {showTitle ?
-        (
-        <h1 onClick={handleClick} id="title" className="title" style={style}>
-          {title}
-        </h1>
-        ) :
-        
-        (
-        <p>Pas de title</p>
-        )
-      }
-
-      <input type="text" />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum alias
-        optio totam, vel harum autem saepe adipisci amet cumque et nam maxime
-        aliquam eius eum explicabo fuga. Explicabo, illo veniam.
-      </p>
-
-      <ul>
-        {todos.map(todo => (<li key={todo}>{todo}</li>))}
-      </ul>
+      <Title color="blue" />
+      <Title color="blue">Mon composant</Title>
+      <Title color="blue" hidden>Mon composant</Title>
+     <h1 id="title" className="title" autoFocus>Bonjour les gens</h1>
+     {todos.map( todo => (<li key={todo}>{todo}</li>))}
     </>
+   
   );
 }
 
-function Title({color , children , hidden , ...props}){
-  if(hidden){
-    return null
-  }
-  return <h1 style={{color:color}} {...props}>{children}</h1>
+function Title({color}){
+  return <h1 style={{color: color}}>Bonjour les gens</h1>
 }
+
+function Title({color , children}){
+  return <h1 style={{color: color}}>{children}</h1>
+}
+
+function Title({color , children , hidden}){
+
+  if(hidden){
+    return null;
+  }
+  return <h1 style={{color: color}}>{children}</h1>
+}
+
+// Le spred operator
+
+function Title({color , children , hidden}){
+
+  if(hidden){
+    return null;
+  }
+  const prpos = {
+    id : 'monid',
+    className :'maclass'
+  }
+  return <h1 style={{color: color}} {...pros}>{children}</h1>
+}
+
 
 export default App;
