dbca62cda39d03c234387f49a1632093b9eb5581
dzeko003 <berenismassamba@gmail.com>
2025-04-02 07:57:46 +0100

    les champs contrôlés par react

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,28 @@
 import { useState } from "react";
 
+/**
+ * defaultValue permet de fixer une valeur initiale au composant
+ * 
+ */
+
 function App() {
  
-  /**
-   * On branche event listener (ecouteur d'evenement)
-   * Les hook ne peuvent pas être déclaré dans une condition
-   * 
-   */
+ const [firstName , setFirstName] = useState('john Doe')
 
-  const [count , setCount] = useState(0);
+ const handleChange = (e) => {
+    setFirstName(e.target.value)
+ }
 
-  const [person , setPerson] = useState({
-    firstName: 'john',
-    lastName : 'Doe',
-    age : 18 
-  })
-
-  const increment = () => {
-    setCount((count) => count + 1 );
-  }
-
-  const incrementAge = () => {
-    setPerson({...person , age : person.age + 1})
-  }
+ const reset = () => {
+    setFirstName('')
+ }
 
   return (
-    <>
-     <p>compteur : {count} </p>
-     <button onClick={increment}>Incrémenter</button>
-
-     <p>Age de {person.firstName} : {person.age} </p>
-     <button onClick={incrementAge}>Augmenter l'âge</button>
-
-    </>
+  <form>
+    <input type="text" name="firstname" value={firstName} onChange={handleChange} />
+    {firstName}
+    <button type="button" onClick={reset}>reset</button>
+  </form>
    
   );
 }
